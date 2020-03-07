76294d823493ae20495fc45f411a68331b4a5b68
Jeunesse Doree Burce <jdeburce@gmail.com>
2020-03-08 01:56:29 +0800

    Added a service, data service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, forkJoin } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private vehiclesSource = new BehaviorSubject([]);
+  vehicles = this.vehiclesSource.asObservable();
+  private coordSource = new BehaviorSubject([0, 0]);
+  coord = this.coordSource.asObservable();
+
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  fetchAllVehicles() {
+    const observables = [];
+    for (let i = 0; i < 6; i++) {
+      observables.push(
+        this.http.get(`http://cndlunarlocator.herokuapp.com/vehicles/${i}/locate.json`)
+      );
+    }
+
+    forkJoin(observables)
+      .subscribe(vehicles => 
+        this.vehiclesSource.next(
+          vehicles.sort((v1: any, v2: any) => 
+            v1.power_level_percent - v2.power_level_percent
+          )
+        )
+      )
+  }
+
+  updateCoord(lat, long) {
+    this.coordSource.next([lat, long]);
+  }
+}
