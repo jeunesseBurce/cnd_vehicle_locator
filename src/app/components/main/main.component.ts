cc5ad3015d8b6713170b93eeb1feae5285bd7de2
Jeunesse Doree Burce <jdeburce@gmail.com>
2020-03-08 01:56:03 +0800

    Added updates to main component, used observables for data from API

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,26 +1,21 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { VehiclesService } from './../../services/vehicles.service';
+import { DataService } from './../../services/data.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
 export class MainComponent implements OnInit {
   vehicleID;
-  vehicles = [];
   
   constructor(
-    private vehiclesService: VehiclesService
+    private data: DataService
   ) { }
 
   ngOnInit(): void {
-    var i;
-    for (i = 0; i < 6; i++) {
-      this.vehiclesService.getVehicle(i).subscribe((data: any[])=>{
-        this.vehicles.push(data);
-      });
-    }
+
   }
 
   title = 'Lunar Locator';
